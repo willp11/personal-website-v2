a842a3b344996559f9fb951eacc734124a0d46a7
willp11 <williampage11@yahoo.co.uk>
2022-10-12 15:35:04 +0700

    added intersection observer to about section

--- components/about.tsx
+++ components/about.tsx
@@ -1,5 +1,12 @@
 import { PlusCircleIcon } from "@heroicons/react/24/outline";
 import CodingAnimation from "./codingAnimation";
+import {useRef, useState, useEffect} from 'react';
+
+const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5
+}
 
 const technologies = [
     "JavaScript (ES6+)",
@@ -14,6 +21,27 @@ const technologies = [
 
 export default function About() {
 
+    // when section intersects the window, transitions in
+    const containerRef = useRef(null);
+    const [isVisible, setIsVisible] = useState(false);
+
+    const callbackFunction = (entries: any) => {
+        const [ entry ] = entries;
+        if (entry.isIntersecting) setIsVisible(true);
+    }
+
+    useEffect(()=>{
+        const observer = new IntersectionObserver(callbackFunction, options);
+        if (containerRef.current) observer.observe(containerRef.current);
+        return () => {
+            if (containerRef.current) observer.unobserve(containerRef.current);
+        }
+    }, [])
+
+    let className="w-full h-full opacity-0 -translate-x-full transition in-ease-out duration-1000";
+    if (isVisible) className="w-full h-full opacity-100 -translate-x-0 transition in-ease-out duration-1000 flex flex-col md:flex-row pb-8"
+
+    // List of technologies
     const tech_list = technologies.map((tech)=>{
         return (
             <div className="flex items-center">
@@ -24,18 +52,21 @@ export default function About() {
     })
 
     return (
-        <div className="w-full max-w-[1000px] mx-auto flex flex-col md:flex-row pb-8">
-            <div className="md:pr-16">
-                <h2>
-                    <span className="text-2xl text-teal-300" style={{fontFamily: 'Ubuntu-Mono'}}>01. </span>
-                    <span className="text-3xl font-bold text-slate-300 after:h-[1px] after:w-[100px] xs:after:w-[200px] sm:after:w-[300px] after:inline after:absolute after:mt-5 after:ml-5 after:bg-slate-600">About Me</span>
-                </h2>
-                <p className="w-full max-w-[500px] text-slate-400 pt-8 pb-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                <ul className="w-full max-w-[500px] grid grid-cols-2">
-                    {tech_list}
-                </ul>
+        <section ref={containerRef} className="w-full min-h-[500px] max-w-[1000px] mx-auto">
+            <div className={className}>
+                <div className="md:pr-16">
+                    <h2>
+                        <span className="text-2xl text-teal-300" style={{fontFamily: 'Ubuntu-Mono'}}>01. </span>
+                        <span className="text-3xl font-bold text-slate-300 after:h-[1px] after:w-[100px] xs:after:w-[200px] sm:after:w-[300px] after:inline after:absolute after:mt-5 after:ml-5 after:bg-slate-600">About Me</span>
+                    </h2>
+                    <p className="w-full max-w-[500px] text-slate-400 pt-8 pb-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <p className="w-full max-w-[500px] text-slate-400">Here are some of the technologies I have been working with recently:</p>
+                    <ul className="w-full max-w-[500px] grid grid-cols-2">
+                        {tech_list}
+                    </ul>
+                </div>
+                <CodingAnimation />
             </div>
-            <CodingAnimation />
-        </div>
+        </section>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,36 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Hero from '../components/hero';
 import About from '../components/about';
+import {useRef, useState, useEffect} from 'react';
+
+const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1
+}
 
 const Home: NextPage = () => {
+
+    // const containerRef = useRef(null);
+    // const [isVisible, setIsVisible] = useState(false);
+
+    // const callbackFunction = (entries: any) => {
+    //     const [ entry ] = entries;
+    //     if (entry.isIntersecting) setIsVisible(true);
+    //     console.log(entry)
+    // }
+
+    // useEffect(()=>{
+    //     const observer = new IntersectionObserver(callbackFunction, options);
+    //     if (containerRef.current) observer.observe(containerRef.current);
+    //     return () => {
+    //         if (containerRef.current) observer.unobserve(containerRef.current);
+    //     }
+    // }, [containerRef.current])
+
+    // let className="w-full h-full -translate-x-full opacity-0 transition in-ease-out duration-500";
+    // if (isVisible) className="w-full h-full -translate-x-0 opacity-100 transition in-ease-out duration-500"
+
     return (
         <div>
             <Head>
@@ -11,7 +39,9 @@ const Home: NextPage = () => {
             </Head>
             <div>
                 <Hero />
-                <About />
+                {/* <div ref={containerRef} className={className}> */}
+                    <About />
+                {/* </div> */}
             </div>
         </div>
     )
