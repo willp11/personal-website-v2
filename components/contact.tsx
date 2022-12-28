637093194b1cc6bf2c749baaabb6cae035021424
willp11 <williampage11@yahoo.co.uk>
2022-12-28 23:11:33 +0700

    added social media and email links in contact me section

--- components/contact.tsx
+++ components/contact.tsx
@@ -20,17 +20,19 @@ export default function Contact() {
                     <p className="text-teal-300 text-xl" style={{fontFamily: 'Ubuntu-Mono'}}>What's next?</p>
                     <h2 className="text-slate-300 text-5xl md:text-6xl font-bold">Get in Touch</h2>
                     <p className="w-full max-w-[500px] text-lg text-slate-300 mt-4">I'm always looking for new opportunities, my inbox is always open. For any enquiries feel free to send me an e-mail, find me on Twitter or LinkedIn!</p>
-                    <div
-                        style={{fontFamily: "Ubuntu-Mono"}}
-                        className="w-32 text-center text-teal-300 border border-teal-300 px-4 py-2 mt-4 rounded-md hover:bg-slate-800 transition duration-500 ease-in-out cursor-pointer"
-                    >
-                        Send Email
-                    </div>
+                    <a href="mailto: will_page_dev@proton.me" target="_blank">
+                        <div
+                            style={{fontFamily: "Ubuntu-Mono"}}
+                            className="w-32 text-center text-teal-300 border border-teal-300 px-4 py-2 mt-4 rounded-md hover:bg-slate-800 transition duration-500 ease-in-out cursor-pointer"
+                        >
+                            Send Email
+                        </div>
+                    </a>
                 </div>
                 <div className="flex justify-center items-center mt-8">
-                    <TwitterLogo size="medium" />
-                    <span className="mx-8"><GithubLogo size="medium" /></span>
-                    <LinkedinLogo size="medium" />
+                    <a href="https://twitter.com/will_page_dev" target="_blank"><TwitterLogo size="medium" /></a>
+                    <span className="mx-8"><a href="https://github.com/willp11" target="_blank"><GithubLogo size="medium" /></a></span>
+                    <a href="https://www.linkedin.com/in/william-page-80a063260" target="_blank"><LinkedinLogo size="medium" /></a>
                 </div>
             </div>
         </Observer>
